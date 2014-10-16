56b07939031f4b14d31c5a9f4d550b531955e447
Weihuang Wong <weihuang.wong@gmail.com>
2014-10-15 23:16:21 -0400

    Added test script

---
+++ test1.R
@@ -0,0 +1,33 @@
+## A script to test out parallelisation
+## on the engage cluster
+
+## Libraries
+require(foreach)
+require(doMPI)
+require(iterators)
+
+## Init MPI backend
+cl <- startMPIcluster()
+registerDoMPI(cl)
+
+## Create data
+D <- lapply(1:10, function(...) {
+    D <- as.data.frame(matrix(rnorm(1e7), ncol=10))
+    names(D) <- LETTERS[1:10]
+    return(D)
+})
+
+## Function to run
+regress <- function(x) {
+    formula <- as.formula(paste("A ~ ", paste(LETTERS[2:10], collapse=" + "), sep=""))
+    coefs <- lm(formula, data=x)$coefficients
+    return(coefs)
+}
+
+## Run
+system.time(coefs <- foreach(a=iter(D), .combine='cbind') %dopar% regress(a))
+print(coefs)
+
+## Cleanup
+closeCluster(cl)
+mpi.quit()
